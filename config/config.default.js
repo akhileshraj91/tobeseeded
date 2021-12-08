7176355f9d8b2b163743837cb537e719072ca32c
Akhilesh Raj <akhileshraj@Akhileshs-Mac-mini.local>
2021-12-07 23:46:03 -0600

    jointjs added

--- config/config.default.js
+++ config/config.default.js
@@ -8,6 +8,10 @@ const mongoHost = process.env.MONGO_HOST || '127.0.0.1';
 config.mongo.uri = `mongodb://${mongoHost}:27017/webgme_dcrypps`;
 config.plugin.allowServerExecution = true;
 
+config.seedProjects.defaultProject = 'tobeseeded';
+
+config.requirejsPaths['jointjs'] = './node_modules/jointjs/dist/joint.min';
+config.requirejsPaths['lodash'] = './node_modules/lodash/lodash.min';
 
 validateConfig(config);
 module.exports = config;
