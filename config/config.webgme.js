e72bf2456b442f3ca84a59631074c3a36ad2cc3f
Akhilesh Raj <akhileshraj@Akhileshs-Mac-mini.local>
2021-12-09 15:43:09 -0600

    codes updated before class on Thursday

--- config/config.webgme.js
+++ config/config.webgme.js
@@ -9,6 +9,7 @@ var config = require('webgme/config/config.default'),
 // The paths can be loaded from the webgme-setup.json
 config.plugin.basePaths.push(__dirname + '/../src/plugins');
 config.seedProjects.basePaths.push(__dirname + '/../src/seeds/tobeseeded');
+config.seedProjects.basePaths.push(__dirname + '/../src/seeds/StateMachine');
 
 
 
